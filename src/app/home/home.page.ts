6daa36fa751e9307f385277d9768b0c144c8dbde
mart8763 <mart.vallejos@duocuc.cl>
2024-09-12 14:20:55 -0300

    spinner

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavigationExtras, Router } from '@angular/router';
+import { AnimationController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -15,40 +16,48 @@ export class HomePage {
 
   // Variable para controlar la visibilidad de la contraseña
   mostrarPassword: boolean = false;
-  
+
   mensaje = '';
 
-  constructor(private router: Router) {}
+  // Para la animacion de carga
+  spinner = false;
+
+  constructor(private router: Router, private animationController: AnimationController) { }
 
   // Funcion para alternar la visibilidad de la contraseña
   togglePassword1Visibility() {
     this.mostrarPassword = !this.mostrarPassword;
   }
 
+  cambiarSpinner() {
+    this.spinner = !this.spinner;
+  }
   validar() {
     if (this.user.username.length != 0) {
       if (this.user.password.length != 0) {
-        //Funciona
-        //this.mensaje = 'Conexion exitosa';
+        this.mensaje = 'Conexion exitosa';
         let navigationExtras: NavigationExtras = {
           state: {
             username: this.user.username,
             password: this.user.password,
           },
         };
+        this.cambiarSpinner();
+        /* setTimeout = permite generar un pequeño delay para realizar la accion */
+        setTimeout(() => {
 
-        this.router.navigate(['/perfil'], navigationExtras);
-        
-        } else {
-          console.log('Contraseña vacia');
-          this.mensaje = 'Contraseña vacia';
-          //No funciona
-        }
+          this.router.navigate(['/perfil'], navigationExtras);
+          this.cambiarSpinner();
+          this.mensaje = "";
+        }, 3000);
       } else {
-        console.log('Usuario vacio');
-        this.mensaje = 'Usuario Vacio';
-        //Tampoco funciona
+        console.log('Contraseña vacia');
+        this.mensaje = 'Contraseña vacia';
       }
+    } else {
+      console.log('Usuario vacio');
+      this.mensaje = 'Usuario Vacio';
+    }
   }
 
 }
